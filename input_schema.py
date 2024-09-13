bcbe7679cf1a619442c67a4a4c8acc9267a7b4c1
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-13 12:07:59 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -3,6 +3,18 @@ INPUT_SCHEMA = {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
-        'example': ["What is Quantum Computing?"]
+        'example': [""a black cat with glowing eyes, cute, adorable, disney, pixar, highly detailed, 8k"]
+    },
+    "img_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/inpaint.png"]
+    },
+    "mask_url": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [1],
+        'example': ["https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/inpaint_mask.png"]
     }
 }
